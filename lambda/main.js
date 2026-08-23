f6a82650c368bdcb56c1cfa54e9dffb6f0b49671
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-08-23 15:29:48 +0530

    add versioning!

--- lambda/main.js
+++ lambda/main.js
@@ -1,10 +1,12 @@
+import "dotenv/config"
 
 exports.handler = async () => {
+    const VERSION = process.env.VERSION;
     return {
         statusCode: 200,
         body: JSON.stringify({
             msg: "Hello World!",
-            version: "1.0.0",
+            version: VERSION,
             timestamp: new Date().toISOString()
         })
     };
--- lib/sandbox-github-actions-aws-nodejs-stack.ts
+++ lib/sandbox-github-actions-aws-nodejs-stack.ts
@@ -1,16 +1,21 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as dotenv from "dotenv"
 
 export class SandboxGithubActionsAwsNodejsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    dotenv.config();
 
     const lambdaFunction = new lambda.Function(this, "LambdaFunction", {
       runtime: lambda.Runtime.NODEJS_LATEST,
       code: lambda.Code.fromAsset("lambda"),
-      handler: "main.handler"
+      handler: "main.handler",
+      environment: {
+        VERSION: process.env.VERSION || "0.0"
+      }
     });
 
 
